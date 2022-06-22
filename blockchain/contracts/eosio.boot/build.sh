32fb724196ecb2afe0ac09bd84269d86c4cfb55a
Jack Tanner <jack@jackandtheblockstalk.com>
2022-06-22 15:40:56 +0200

    Fixed build scripts

--- blockchain/contracts/eosio.boot/build.sh
+++ blockchain/contracts/eosio.boot/build.sh
@@ -2,9 +2,9 @@
 
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-DOCKER_WORKING_DIR="/tmp"
+DOCKER_WORKING_DIR="/contract"
 CONTRACT_NAME="eosio.boot"
-docker run -v "${PARENT_PATH}/contracts/${CONTRACT_NAME}:${DOCKER_WORKING_DIR}"\
+docker run -v "${PARENT_PATH}:${DOCKER_WORKING_DIR}"\
     eostudio/eosio.cdt:v1.8.1\
     eosio-cpp\
     -abigen\
