c75f0807acdb3fa6d264d31d88657906f3e865a2
Jack Tanner <jack@jackandtheblockstalk.com>
2022-06-22 16:58:57 +0200

    Lint script

---
+++ scripts/lint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Make sure working dir is same as this dir, so that script can be excuted from another working directory
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+SCRIPTS=$(find . -name "*.sh" -print0)
+docker run -v "${PARENT_PATH}/..:/var/repo" koalaman/shellcheck shellcheck -a -S style /var/repo/app.sh
