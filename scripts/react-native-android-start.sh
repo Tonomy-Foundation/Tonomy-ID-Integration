971590118ca20c8802ceb1a412d007f7c49855b3
Jack Tanner <jack@jackandtheblockstalk.com>
2022-06-30 18:12:15 +0200

    Simplified react-native-android-start.sh

--- scripts/react-native-android-start.sh
+++ scripts/react-native-android-start.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
+
+# Make sure the android emulator is running and connect to it
 cd /home/node
 ./dockerize -wait tcp://android-emulator:5555
-./platform-tools/adb connect android-emulator:5555
+sleep 2
+adb connect android-emulator:5555
 
-echo "Waiting 60 second for Android to startup"
 echo "Open your browser to http://localhost:6080 to open emulator"
-sleep 60
-echo "Waiting... done"
 
 cd /var/repo/Tonomy-ID
-#npm run start &
-#sleep 5
+npm run start &
+sleep 2
 npm run android
