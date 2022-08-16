7466368652657a41c95b5f38af1daf55e8efbd36
Jack Tanner <jack@jackandtheblockstalk.com>
2022-08-16 09:57:36 +0200

    using Authority class

--- initialize/create-account.ts
+++ initialize/create-account.ts
@@ -1,15 +1,15 @@
 import { EosioUtil } from 'tonomy-id-sdk';
-const { createKeyAuthoriy, addCodePermission, publicKey } = EosioUtil;
-import { EosioContract } from 'tonomy-id-sdk';
+const { publicKey } = EosioUtil;
+import { Authority, EosioContract } from 'tonomy-id-sdk';
 
 const eosioContract: EosioContract = EosioContract.Instance;
 
 async function createAccount({ account }) {
-    const ownerAuth = createKeyAuthoriy(publicKey.toString());
+    const ownerAuth = Authority.fromKey(publicKey.toString());
 
-    // need to add the eosio.code authority as well
-    // https://developers.eos.io/welcome/v2.1/smart-contract-guides/adding-inline-actions#step-1-adding-eosiocode-to-permissions
-    const activeAuth = addCodePermission(ownerAuth, account)
+    const activeAuth = Authority.fromKey(publicKey.toString());
+    // need to add the eosio.code authority as well so that it can call eosio from the smart contract
+    activeAuth.addCodePermission("eosio");
 
     console.log(`Creating new account ${account}`)
     await eosioContract.newaccount("eosio", account, ownerAuth, activeAuth);
