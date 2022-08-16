3344d938c6b284424bf2128d5c5ce6f326be6879
Jack Tanner <jack@jackandtheblockstalk.com>
2022-08-16 11:15:21 +0200

    Added eosio.code during bootstrap

--- initialize/create-account.ts
+++ initialize/create-account.ts
@@ -8,10 +8,10 @@ async function createAccount({ account }) {
     const ownerAuth = Authority.fromKey(publicKey.toString());
 
     const activeAuth = Authority.fromKey(publicKey.toString());
-    // need to add the eosio.code authority as well so that it can call eosio from the smart contract
-    activeAuth.addCodePermission("eosio");
 
     console.log(`Creating new account ${account}`)
+    // need to add the eosio.code authority as well so that it can call eosio from the smart contract
+    activeAuth.addCodePermission(account);
     await eosioContract.newaccount("eosio", account, ownerAuth, activeAuth);
 }
 
