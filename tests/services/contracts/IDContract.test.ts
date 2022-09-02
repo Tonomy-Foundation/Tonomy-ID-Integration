2b4d8331cb267b230167820ca4fdf5341a17863c
rebal67 <rebalalh@gmail.com>
2022-09-02 11:19:56 +0200

    fix test

--- tests/services/contracts/IDContract.test.ts
+++ tests/services/contracts/IDContract.test.ts
@@ -16,7 +16,7 @@ describe("IDContract tests", () => {
         expect(idInfo.account_name).toEqual(user.accountName.toString());
         expect(idInfo.username_hash).toEqual(sha256(user.username));
         expect(idInfo.status).toEqual(0); // 0 = Creating
-        expect(idInfo.type).toEqual(0); // 0 = Person
+        expect(idInfo.account_name[0]).toEqual('p'); // 0 = Person
         // expect(idInfo.password_salt).toEqual(user.salt); // TODO uncomment when salt implemented properly in User.ts
 
 
