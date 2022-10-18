f71c4742cb0ac0c7c41fe5b74f9f170bc73bf6f4
Jack Tanner <jack@jackandtheblockstalk.com>
2022-10-18 14:59:27 +0200

    staging instruction commands

---
+++ scripts/staging.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Commands for the staging server
+
+git clone https://github.com/Tonomy-Foundation/Tonomy-ID-Integration.git
+cd Tonomy-ID-Integration
+git checkout development
+./apps.sh gitinit
+./apps.sh install
+./apps.sh init
+./apps.sh start
+
+# next time, to upgrade
+cd Tonomy-ID-Integration
+./apps.sh stop
+git pull
+cd Tonomy-ID
+git pull
+cd ../Tonomy-Contracts
+git pull
+cd ../Tonomy-ID-SDK
+git pull
+cd ../Tonomy-ID-Demo
+git pull
+cd ../
+./apps.sh init
+./apps.sh start
