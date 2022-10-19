b43e3a9f54ccd7a71ecfd971bdd0b4cba2ef9923
Jack Tanner <jack@jackandtheblockstalk.com>
2022-10-19 10:24:30 +0200

    using nginx in docker

---
+++ nginx/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "updating /etc/hosts to redirect urls to localhost for testing"
+
+echo "127.0.0.1 blockchainapi.staging.tonomy.foundation" | sudo tee -a /etc/hosts
+echo "127.0.0.1 website.staging.tonomy.foundation" | sudo tee -a /etc/hosts
+echo "127.0.0.1 expo.staging.tonomy.foundation" | sudo tee -a /etc/hosts
