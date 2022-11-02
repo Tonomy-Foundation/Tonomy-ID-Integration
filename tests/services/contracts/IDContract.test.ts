3f72eb820e2148f44ee5e67a804fe06dbefeed6c
Jack Tanner <jack@jackandtheblockstalk.com>
2022-11-02 18:16:04 +0100

    Fixed unit tests for async storage

--- tests/services/contracts/IDContract.test.ts
+++ tests/services/contracts/IDContract.test.ts
@@ -1,3 +1,4 @@
+import { Checksum256 } from '@greymass/eosio';
 import { IDContract, sha256 } from 'tonomy-id-sdk';
 import { createRandomID } from '../../util/user';
 
@@ -11,20 +12,24 @@ describe('IDContract class', () => {
     test('getAccountTonomyIDInfo(): Fetch ID details of a user', async () => {
         const { user } = await createRandomID();
 
-        // get by username
-        let idInfo = await idContract.getAccountTonomyIDInfo(user.storage.accountName);
+        const accountName = await user.storage.accountName;
+        const username = await user.storage.username;
+        const salt = await user.storage.salt;
 
-        expect(idInfo.account_name).toEqual(user.storage.accountName);
-        expect(idInfo.username_hash.toString()).toEqual(sha256(user.storage.username));
+        // get by account name
+        let idInfo = await idContract.getAccountTonomyIDInfo(accountName);
+
+        expect(idInfo.account_name).toEqual(accountName);
+        expect(idInfo.username_hash.toString()).toEqual(Checksum256.from(sha256(username)).toString());
         expect(idInfo.status).toEqual(0); // 0 = Creating. TODO turn into enum string
         // expect(idInfo.type).toEqual(0); // 0 = Person // TODO bring back type property (as enum string) based on account_name[0] character
         expect(idInfo.account_name.toString()[0]).toEqual('p'); // p = person
-        expect(idInfo.password_salt).toEqual(user.storage.salt);
+        expect(idInfo.password_salt).toEqual(salt);
         expect(idInfo.version).toBe(1);
 
         // get by username
-        idInfo = await idContract.getAccountTonomyIDInfo(user.storage.username);
-        expect(idInfo.account_name.toString()).toEqual(user.storage.accountName.toString());
-        expect(idInfo.username_hash.toString()).toEqual(sha256(user.storage.username));
+        idInfo = await idContract.getAccountTonomyIDInfo(username);
+        expect(idInfo.account_name.toString()).toEqual(accountName.toString());
+        expect(idInfo.username_hash.toString()).toEqual(sha256(username));
     });
 });
