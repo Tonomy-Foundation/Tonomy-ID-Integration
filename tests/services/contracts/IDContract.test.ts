c37fc4ddb8ecdbc44509d1967c0ac5b5cf4eb540
Jack Tanner <jack@jackandtheblockstalk.com>
2022-11-14 17:35:39 +0100

    changed to use getPerson()

--- tests/services/contracts/IDContract.test.ts
+++ tests/services/contracts/IDContract.test.ts
@@ -8,7 +8,7 @@ describe('IDContract class', () => {
         jest.setTimeout(60000);
     });
 
-    test('getAccountTonomyIDInfo(): Fetch ID details of a user', async () => {
+    test('gePerson(): Fetch ID details of a user', async () => {
         const { user } = await createRandomID();
 
         const accountName = await user.storage.accountName;
@@ -16,18 +16,18 @@ describe('IDContract class', () => {
         const salt = await user.storage.salt;
 
         // get by account name
-        let idInfo = await idContract.getAccountTonomyIDInfo(accountName);
+        let idInfo = await idContract.gePerson(accountName);
 
         expect(idInfo.account_name).toEqual(accountName);
         expect(idInfo.username_hash.toString()).toEqual(username.usernameHash);
-        expect(idInfo.status).toEqual(0); // 0 = Creating. TODO turn into enum string
+        expect(idInfo.status).toEqual(1); // 1 = READY. TODO turn into enum string
         // expect(idInfo.type).toEqual(0); // 0 = Person // TODO bring back type property (as enum string) based on account_name[0] character
         expect(idInfo.account_name.toString()[0]).toEqual('p'); // p = person
         expect(idInfo.password_salt).toEqual(salt);
         expect(idInfo.version).toBe(1);
 
         // get by username
-        idInfo = await idContract.getAccountTonomyIDInfo(username);
+        idInfo = await idContract.gePerson(username);
         expect(idInfo.account_name.toString()).toEqual(accountName.toString());
         expect(idInfo.username_hash.toString()).toEqual(username.usernameHash);
     });
